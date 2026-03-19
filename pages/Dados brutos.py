b3fabbc94479cf31fc0ebf01e9799e3340a904a7
Alex Nascimento <137247689+alexnascimento1980@users.noreply.github.com>
2026-03-19 16:07:32 -0300

    Adiciona funcionalidade de download em CSV e mensagem de sucesso no dashboard de dados brutos

--- pages/Dados brutos.py
+++ pages/Dados brutos.py
@@ -1,8 +1,21 @@
 import streamlit as st
 import requests
 import pandas as pd
+import time
 
-st.title("DADOS BRUTOS")
+
+@st.cache_data
+def converte_csv(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+
+def mensagem_sucesso():
+    sucesso = st.success('Arquivo baixado com sucesso!', icon="✅")
+    time.sleep(5)
+    sucesso.empty()
+
+
+st.title('DADOS BRUTOS')
 
 url = 'https://labdados.com/produtos'
 
@@ -12,29 +25,10 @@ dados['Data da Compra'] = pd.to_datetime(
     dados['Data da Compra'], format='%d/%m/%Y')
 
 with st.expander('Colunas'):
-    colunas = st.multiselect('Selecione as colunas que deseja visualizar',
-                             list(dados.columns), default=list(dados.columns))
+    colunas = st.multiselect('Selecione as colunas', list(
+        dados.columns), list(dados.columns))
 
-st.sidebar.title("Filtros")
-with st.sidebar.expander('Nome do Produto'):
-    produtos = st.multiselect('Selecione os produtos que deseja visualizar',
-                              dados['Produto'].unique(),
-                              dados['Produto'].unique()
-                              )
-
-with st.sidebar.expander('Preço do Produto'):
-    preco = st.slider('Selecione o preço que deseja visualizar',
-                      0,
-                      5000,
-                      (0, 5000)
-                      )
-
-with st.sidebar.expander('Data da Compra'):
-    data_compra = st.date_input('Selecione a data da compra que deseja visualizar',
-                                (dados['Data da Compra'].min(),
-                                 dados['Data da Compra'].max())
-                                )
-
+st.sidebar.title('Filtros')
 with st.sidebar.expander('Nome do produto'):
     produtos = st.multiselect(
         'Selecione os produtos', dados['Produto'].unique(), dados['Produto'].unique())
@@ -64,17 +58,33 @@ with st.sidebar.expander('Quantidade de parcelas'):
     qtd_parcelas = st.slider(
         'Selecione a quantidade de parcelas', 1, 24, (1, 24))
 
-# Filtros
 query = '''
-    Produto in @produtos and \
-    @preco[0] <= Preço <= @preco[1] and \
-    @data_compra[0] <= `Data da Compra` <= @data_compra[1]
+Produto in @produtos and \
+`Categoria do Produto` in @categoria and \
+@preco[0] <= Preço <= @preco[1] and \
+@frete[0] <= Frete <= @frete[1] and \
+@data_compra[0] <= `Data da Compra` <= @data_compra[1] and \
+Vendedor in @vendedores and \
+`Local da compra` in @local_compra and \
+@avaliacao[0]<= `Avaliação da compra` <= @avaliacao[1] and \
+`Tipo de pagamento` in @tipo_pagamento and \
+@qtd_parcelas[0] <= `Quantidade de parcelas` <= @qtd_parcelas[1]
 '''
+
 dados_filtrados = dados.query(query)
 dados_filtrados = dados_filtrados[colunas]
-
 
 st.dataframe(dados_filtrados)
 
 st.markdown(
-    f'A tabela possui :blue[{dados_filtrados.shape[0]}] linhas e :blue[{dados_filtrados.shape[1]}] colunas.')
+    f'A tabela possui :blue[{dados_filtrados.shape[0]}] linhas e :blue[{dados_filtrados.shape[1]}] colunas')
+
+st.markdown('Escreva um nome para o arquivo')
+coluna1, coluna2 = st.columns(2)
+with coluna1:
+    nome_arquivo = st.text_input(
+        '', label_visibility='collapsed', value='dados')
+    nome_arquivo += '.csv'
+with coluna2:
+    st.download_button('Fazer o download da tabela em csv', data=converte_csv(
+        dados_filtrados), file_name=nome_arquivo, mime='text/csv', on_click=mensagem_sucesso)
